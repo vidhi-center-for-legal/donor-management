cbc7742599e32d048477c55a6f36478b5c67b05a
tanuushree <tanushree.wrk@gmail.com>
2024-07-01 15:28:48 +0530

    Completed financial overview report

--- donor_management/donor_management/doctype/financial_overview/financial.py
+++ donor_management/donor_management/doctype/financial_overview/financial.py
@@ -2,24 +2,31 @@ import frappe
 from frappe import _
 
 @frappe.whitelist(allow_guest=True)
+def getFundsByFinancialYear(financial_year):
+       return {
+              "committed": getCommittedFundsByFinancialYear(financial_year),
+              "expected" : getExpectedFundsByFinancialYear(financial_year)     
+            }
+    
+def getExpectedFundsByFinancialYear(financial_year):
 
-def getFundsByFinancialYearAndFunderType(financial_year,fund_type):
-    if fund_type == 'committed':
         sql_query = """
-            SELECT round(sum(tranche_amount)) as amount
-            FROM `tabDonation`
-            WHERE financial_year = %(financial_year)s
-        """
-        response_key = "amount"
-    elif fund_type == 'expected':
-        sql_query = """
-            SELECT round(sum(amount)) as ExpectedAmount
+            SELECT IFNULL(round(sum(amount)),0) as Expected_Amount
             FROM `tabFunder`
             WHERE funder_status = 'expected' AND financial_year = %(financial_year)s
         """
-        response_key = "ExpectedAmount"
-    else:
-        return None
+        response = frappe.db.sql(sql_query, {"financial_year": financial_year}, as_dict=True)
+        return response[0]["Expected_Amount"]
+        
 
-    response = frappe.db.sql(sql_query, {"financial_year": financial_year}, as_dict=True)
-    return response[0][response_key] if response else None
+
+
+def getCommittedFundsByFinancialYear(financial_year):
+        sql_query = """
+            SELECT IFNULL(round(sum(tranche_amount)),0) as Commited_Amount
+            FROM `tabDonation`
+            WHERE financial_year = %(financial_year)s
+        """
+        response = frappe.db.sql(sql_query, {"financial_year": financial_year}, as_dict=True)
+        return response[0]["Commited_Amount"]
+     
--- donor_management/donor_management/doctype/financial_overview/financial_overview.js
+++ donor_management/donor_management/doctype/financial_overview/financial_overview.js
@@ -1,38 +1,27 @@
 frappe.ui.form.on('Financial Overview', {
-    after_save: function(frm) {
+    before_save: function(frm) {
         
         frappe.call({
-            method: "donor_management.donor_management.doctype.financial_overview.financial.getFundsByFinancialYearAndFunderType",
+            method: "donor_management.donor_management.doctype.financial_overview.financial.getFundsByFinancialYear",
            
             args: {
                 
                 financial_year: frm.doc.name,
-                fund_type: 'committed',
                 
             },
+            freeze: true,
             callback: function(data) {
-                frm.set_value("total_funds_received",data.message);   
+                console.log(data.message);
+                frm.set_value("total_funds_received",data.message["committed"]);   
                 frm.set_value("fr_targets", frm.doc.budget_amount*.15);
                 frm.set_value("total_fr_budget", frm.doc.budget_amount+frm.doc.fr_targets);
                 frm.set_value("fr__gap", frm.doc.total_fr_budget-frm.doc.mou_billing_expected);
-                frm.set_value("budget_gap", frm.doc.budget_amount-frm.doc.mou_billing_expected);         
+                frm.set_value("budget_gap", frm.doc.budget_amount-frm.doc.mou_billing_expected); 
+                frm.set_value("funds_committed_and_expected",data.message["expected"]+data.message["committed"]);  
+                frm.set_value("expected_income",frm.doc.total_funds_received+frm.doc.funds_committed_and_expected+frm.doc.mou_billing_expected+frm.doc.mou_billing_raised+frm.doc.interest_income)
+              
             }
         });
-        frappe.call({
-            method: "donor_management.donor_management.doctype.financial_overview.financial.getFundsByFinancialYearAndFunderType",
-           
-            args: {
-                financial_year: frm.doc.name,
-                fund_type: 'expected'
-            },
-            callback: function(data) {
-                frm.set_value("funds_committed_and_expected",data.message+frm.doc.total_funds_received);
-            }
-        });
-        
-        frm.set_value("expected_income",frm.doc.total_funds_received+frm.doc.funds_committed_and_expected+frm.doc.mou_billing_expected+frm.doc.mou_billing_raised+frm.doc.interest_income)
-        frm.save();
-
         
     }
 });
