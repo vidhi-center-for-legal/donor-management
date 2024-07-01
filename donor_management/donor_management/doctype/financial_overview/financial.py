61f0920749dbf50b99a22ec6e6fc1d1b43703a73
tanuushree <tanushree.wrk@gmail.com>
2024-07-01 14:16:51 +0530

    Completed financial overview report

--- donor_management/donor_management/doctype/financial_overview/financial.py
+++ donor_management/donor_management/doctype/financial_overview/financial.py
@@ -1,25 +1,25 @@
 import frappe
 from frappe import _
 
-
 @frappe.whitelist(allow_guest=True)
-def getTotalCommittedFundsByFinancialYear(financial_year):
-    sql_query = """
-        SELECT round(sum(tranche_amount)) as amount
-        FROM `tabDonation`
-        WHERE  financial_year = %(financial_year)s
-    """
+
+def getFundsByFinancialYearAndFunderType(financial_year,fund_type):
+    if fund_type == 'committed':
+        sql_query = """
+            SELECT round(sum(tranche_amount)) as amount
+            FROM `tabDonation`
+            WHERE financial_year = %(financial_year)s
+        """
+        response_key = "amount"
+    elif fund_type == 'expected':
+        sql_query = """
+            SELECT round(sum(amount)) as ExpectedAmount
+            FROM `tabFunder`
+            WHERE funder_status = 'expected' AND financial_year = %(financial_year)s
+        """
+        response_key = "ExpectedAmount"
+    else:
+        return None
+
     response = frappe.db.sql(sql_query, {"financial_year": financial_year}, as_dict=True)
-    return response[0]["amount"] if response else None
-
-
-
-@frappe.whitelist(allow_guest=True)
-def getTotalExpectedFundsByFinancialYear(financial_year):
-    sql_query = """
-        SELECT round(sum(tranche_amount)) as amount
-        FROM `tabDonation`
-        WHERE  financial_year = %(financial_year)s
-    """
-    response = frappe.db.sql(sql_query, {"financial_year": financial_year}, as_dict=True)
-    return response[0]["amount"] if response else None
+    return response[0][response_key] if response else None
--- donor_management/donor_management/doctype/financial_overview/financial_overview.js
+++ donor_management/donor_management/doctype/financial_overview/financial_overview.js
@@ -1,22 +1,37 @@
 frappe.ui.form.on('Financial Overview', {
-    refresh: function(frm) {
-        frm.set_value("fr_targets", frm.doc.budget_amount*.15);
-        frm.set_value("total_fr_budget", frm.doc.budget_amount+frm.doc.fr_targets);
-        frm.set_value("fr__gap", frm.doc.total_fr_budget-frm.doc.mou_billing_expected);
-        frm.set_value("budget_gap", frm.doc.budget_amount-frm.doc.mou_billing_expected);
+    after_save: function(frm) {
+        
         frappe.call({
-            method: "donor_management.donor_management.doctype.financial_overview.financial.getTotalDonatiosByFinancialYear",
+            method: "donor_management.donor_management.doctype.financial_overview.financial.getFundsByFinancialYearAndFunderType",
+           
+            args: {
+                
+                financial_year: frm.doc.name,
+                fund_type: 'committed',
+                
+            },
+            callback: function(data) {
+                frm.set_value("total_funds_received",data.message);   
+                frm.set_value("fr_targets", frm.doc.budget_amount*.15);
+                frm.set_value("total_fr_budget", frm.doc.budget_amount+frm.doc.fr_targets);
+                frm.set_value("fr__gap", frm.doc.total_fr_budget-frm.doc.mou_billing_expected);
+                frm.set_value("budget_gap", frm.doc.budget_amount-frm.doc.mou_billing_expected);         
+            }
+        });
+        frappe.call({
+            method: "donor_management.donor_management.doctype.financial_overview.financial.getFundsByFinancialYearAndFunderType",
            
             args: {
                 financial_year: frm.doc.name,
+                fund_type: 'expected'
             },
             callback: function(data) {
-                frm.set_value("total_funds_received",data.message);
-                frm.save();
-                
+                frm.set_value("funds_committed_and_expected",data.message+frm.doc.total_funds_received);
             }
         });
-
+        
+        frm.set_value("expected_income",frm.doc.total_funds_received+frm.doc.funds_committed_and_expected+frm.doc.mou_billing_expected+frm.doc.mou_billing_raised+frm.doc.interest_income)
+        frm.save();
 
         
     }
