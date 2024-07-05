97685e0a8cd0ab211024c7c2bae06ed33fec54dc
tanuushree <tanushree.wrk@gmail.com>
2024-07-05 15:48:36 +0530

    fix:removed donor_type field

--- donor_management/donor_management/report/overall_donation_report/overall_donation_report.py
+++ donor_management/donor_management/report/overall_donation_report/overall_donation_report.py
@@ -14,7 +14,7 @@ def execute(filters=None):
         {"label": _("Donor Name"), "fieldname": "donor_name", "fieldtype": "Data", "width": 150},
         {"label": _("Email"), "fieldname": "email", "fieldtype": "Data", "width": 160},
         {"label": _("Latest Donation Date"), "fieldname": "latest_donation_date", "fieldtype": "Date", "width": 150},
-        {"label": _("Donor Type"), "fieldname": "donor_type", "fieldtype": "Data", "width": 150},
+        # {"label": _("Donor Type"), "fieldname": "donor_type", "fieldtype": "Data", "width": 150},
         {"label": _("Total Donation Amount"), "fieldname": "total_donation_amount", "fieldtype": "Currency", "width": 180},
         {"label": _("Total Utilisation Amount"), "fieldname": "total_utilisation_amount", "fieldtype": "Currency", "width": 180},
         {"label": _("Available Donation Amount"), "fieldname": "available_donation_amount", "fieldtype": "Currency", "width": 180},
@@ -25,14 +25,14 @@ def execute(filters=None):
         overall_donations = frappe.get_all(
         "Overall Donation",
         filters={"latest_donation_date": ["between", [from_date, to_date]]},
-        fields=["donor_id", "donor_name", "latest_donation_date", "donor_type", "total_donation", "total_utilisation", "available_donation_amount", "donor_email"],
+        fields=["donor_id", "donor_name", "latest_donation_date", "total_donation", "total_utilisation", "available_donation_amount", "donor_email"],
     )
          
     else:
         overall_donations = frappe.get_all(
         "Overall Donation",
         filters={},
-        fields=["donor_id", "donor_name", "latest_donation_date", "donor_type", "total_donation", "total_utilisation", "available_donation_amount", "donor_email"],
+        fields=["donor_id", "donor_name", "latest_donation_date", "total_donation", "total_utilisation", "available_donation_amount", "donor_email"],
     )
     # Process overall donations data
     for overall_donation in overall_donations:
@@ -41,7 +41,7 @@ def execute(filters=None):
             "donor_name": overall_donation.donor_name,
             "email": overall_donation.donor_email,
             "latest_donation_date": overall_donation.latest_donation_date,
-            "donor_type": overall_donation.donor_type,
+            # "donor_type": overall_donation.donor_type,
             "total_donation_amount": overall_donation.total_donation,
             "total_utilisation_amount": overall_donation.total_utilisation,
             "available_donation_amount": overall_donation.available_donation_amount,
