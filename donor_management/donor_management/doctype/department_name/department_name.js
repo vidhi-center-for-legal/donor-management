7c8706f62f114e88ff4545a7f3bc352ae9e31f98
tanuushree <tanushree.wrk@gmail.com>
2024-07-05 15:51:51 +0530

    Deleted doctypes-agent,leads and dashboard-lead, modified donor,donation and utilisation dashboard

--- donor_management/donor_management/doctype/department_name/department_name.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Tech4Good Community and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Department Name", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- donor_management/donor_management/doctype/udhyam_website_api_setting/test_udhyam_website_api_setting.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Tech4Good Community and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestUdhyamWebsiteAPISetting(FrappeTestCase):
-	pass
